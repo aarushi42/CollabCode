bee383a8d89a0f419290eea0f88f9c103e250cef
Aarushi42 <aarushikoirala207@gmail.com>
2026-03-04 19:18:57 +0530

    get API's

--- src/app.js
+++ src/app.js
@@ -3,14 +3,37 @@ const app = express();
 const connectDB = require("./config/database");
 const User = require("./models/user");
 
+app.use(express.json());
+
+//getting a user on the basis of condition
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send("User not Found");
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(500).send("error occured" + err);
+  }
+});
+
+//get all the user
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(users);
+  } catch (err) {
+    res.status(500).send("error occured again");
+  }
+});
+
 app.post("/signup", async (req, res) => {
   //creating a new instance of User model
-  const user = new User({
-    firstName: "Aarushi",
-    lastName: "Koirala",
-    emailId: "koiaarushi@42.com",
-    password: "aarushi@12",
-  });
+  const user = new User(req.body);
 
   try {
     await user.save();
