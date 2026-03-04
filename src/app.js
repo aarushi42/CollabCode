9d9be62ee088ba9dc5fe62bd7e6455dea4d7061f
Aarushi42 <aarushikoirala207@gmail.com>
2026-03-04 13:42:04 +0530

    exploring routes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,40 @@
 const express = require("express");
 const app = express();
 
+app.get("/user", (req, res) => {
+  res.send({
+    firstName: "aarushi",
+    lastName: "Koirala",
+  });
+});
+
+app.post("/user", (req, res) => {
+  res.send("Data posted successfully");
+});
+
+app.patch("/user", (req, res) => {
+  res.send("Data patched successfully");
+});
+
+app.put("/user", (req, res) => {
+  res.send("Data put successfully");
+});
+
+app.delete("/user", (req, res) => {
+  res.send("Data deleted successfully");
+});
+//the order is v important as it starts mapping from top and then to bottom
+
 app.use("/test", (req, res) => {
-  res.send("Hello to test");
+  res.send("Hello to test"); //anything after /test is handled by this
 });
 
 app.use("/app", (req, res) => {
-  res.send("hello to app");
+  res.send("hello to app"); //anything after /app is handled by this
 });
 
 app.use((req, res) => {
-  res.send("Hello to /");
+  res.send("Hello to /"); //anything after / is handled by this
 });
 
 app.listen(7777, () => {
