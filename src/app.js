920f31cc7ce5f366da563d6cc777516746d2578e
Aarushi42 <aarushikoirala207@gmail.com>
2026-03-04 18:00:53 +0530

    post/signup api created

--- src/app.js
+++ src/app.js
@@ -1,6 +1,24 @@
 const express = require("express");
 const app = express();
 const connectDB = require("./config/database");
+const User = require("./models/user");
+
+app.post("/signup", async (req, res) => {
+  //creating a new instance of User model
+  const user = new User({
+    firstName: "Aarushi",
+    lastName: "Koirala",
+    emailId: "koiaarushi@42.com",
+    password: "aarushi@12",
+  });
+
+  try {
+    await user.save();
+    res.send("User added Successfully");
+  } catch (err) {
+    res.status(401).send("error saving the user" + err.message);
+  }
+});
 
 connectDB()
   .then(() => {
